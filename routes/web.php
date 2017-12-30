7d9f7ddff5afe4d75fa67bdcf62ad23b8b9bb454
Joseph Mancuso <idmann509@gmail.com>
2017-12-29 21:42:11 -0500

    added needed routes to web file

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,26 @@
 
 // Route::get('/', 'WelcomeController@show');
 
+Route::domain('{repo}.orderinghero.com')->group(function () {
+    // show the default version
+    Route::get('/', 'MarkdownController@show');
+
+    // show the default version
+    Route::get('/docs/{slug}', 'MarkdownController@single');
+
+    // show the default version
+    Route::get('/v/{version}', 'MarkdownController@singleVersion');
+    Route::get('/v/{version}/{slug}', 'MarkdownController@version');
+
+    Route::get('/search/{version}', 'MarkdownController@searchVersion');
+});
+
 Route::get('/home', 'HomeController@show');
 Route::get('/home/repo/add/{id}', 'HomeController@repo');
-Route::get('/home/repo/{id}', 'HomeController@detail');
+Route::get('/home/repo/{id}', 'HomeController@detail')->name('repoDetails');
 Route::post('/home/repo/{id}', 'HomeController@detailStore');
+
+Route::post('/repo/download', 'HomeController@download');
 
 
 Route::get('/login/github', 'auth\LoginController@githubToProvider');
@@ -28,17 +44,7 @@ Route::get('/testing', 'MarkdownController@testFetch');
 
 Route::post('/hook', 'UpdateController@hook');
 
-Route::domain('{repo}.orderinghero.com')->group(function () {
-    // show the default version
-    Route::get('/', 'MarkdownController@show');
-
-    // show the default version
-    Route::get('/docs/{slug}', 'MarkdownController@single');
-
-    // show the default version
-    Route::get('/v/{version}', 'MarkdownController@singleVersion');
-    Route::get('/v/{version}/{slug}', 'MarkdownController@version');
-});
 
 
+Route::get('/', 'WelcomeController@show');
 
